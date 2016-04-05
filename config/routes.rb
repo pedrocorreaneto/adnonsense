e89bb3f587d5966ba1fb383029040ae8f21cfe07
Pedro Corrêa <pedro@Blackbird>
2016-04-05 11:16:37 -0300

    Alterações nas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :ads do 
-    resources :targetings
-    resources :creatives
-  end
+  resources :ads  
+  root 'ads#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
